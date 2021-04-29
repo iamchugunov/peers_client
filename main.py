09aafd44374447ece1339d85a211493c63d2b0ed
Alexander Chugunov <San4es_95@mail.ru>
2021-04-29 09:33:38 +0300

    28.04

--- main.py
+++ main.py
@@ -20,14 +20,66 @@
 # loop.close()
 
 import socket
+import json
+
+
+
+with open("config/rf_params.json", "r") as file:
+    rf_params = (json.loads(file.read()))
+
+# list of anchors to configure
+anchors_conf = []
+with open("config/anchors.json", "r") as file:
+    for line in file:
+        anchors_conf.append(json.loads(line))
+
+print(type(anchors_conf[0]))
 
 s = socket.socket()
 # s.connect(('127.0.1.2', 8888))
-s.connect(('192.168.0.196', 5051))
+s.connect(('192.168.1.22', 5051))
 
 while 1:
     data = input()
-    s.sendall(data.encode())
+    if data == "1":
+        for anchor in anchors_conf:
+            data2send = anchor
+            data2send["type"] = "new_anchor"
+            data2send = json.dumps(data2send).encode()
+            s.sendall(len(data2send).to_bytes(4, "little"))
+            s.sendall(data2send)
+
+    if data == "2":
+        data2send = {}
+        data2send["type"] = "relate_to_masters"
+        data2send = json.dumps(data2send).encode()
+        s.sendall(len(data2send).to_bytes(4, "little"))
+        s.sendall(data2send)
+
+    if data == "3":
+        data2send = rf_params
+        data2send["type"] = "rf_config"
+        data2send = json.dumps(data2send).encode()
+        s.sendall(len(data2send).to_bytes(4, "little"))
+        s.sendall(data2send)
+
+    if data == "go":
+        data2send = {}
+        data2send["type"] = "Start"
+        data2send = json.dumps(data2send).encode()
+        s.sendall(len(data2send).to_bytes(4, "little"))
+        s.sendall(data2send)
+
+    if data == "stop":
+        data2send = {}
+        data2send["type"] = "Stop"
+        data2send = json.dumps(data2send).encode()
+        s.sendall(len(data2send).to_bytes(4, "little"))
+        s.sendall(data2send)
+
+
+
+    # s.sendall(data.encode())
     if data == "howmuch":
         print((s.recv(100)).decode())
 
